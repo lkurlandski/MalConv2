c899aca86c7231c92188769719671a4edd0a2f11
lkurlandski <lk3591@g.rit.edu>
2022-10-14 09:37:34 -0400

    cant remember the changes

--- binaryLoader.py
+++ binaryLoader.py
@@ -77,13 +77,14 @@ class RandomChunkSampler(torch.utils.data.sampler.Sampler):
     """
     Samples random "chunks" of a dataset, so that items within a chunk are always loaded together. Useful to keep chunks in similar size groups to reduce runtime. 
     """
-    def __init__(self, data_source, batch_size):
+    def __init__(self, data_source, batch_size, random=True):
         """
         data_source: the souce pytorch dataset object
         batch_size: the size of the chunks to keep together. Should generally be set to the desired batch size during training to minimize runtime. 
         """
         self.data_source = data_source
         self.batch_size = batch_size
+        self.random = random
         
     def __iter__(self):
         n = len(self.data_source)
@@ -93,7 +94,8 @@ class RandomChunkSampler(torch.utils.data.sampler.Sampler):
         # Create blocks
         blocks = [data[i:i+self.batch_size] for i in range(0,len(data),self.batch_size)]
         # shuffle the blocks
-        random.shuffle(blocks)
+        if self.random:
+            random.shuffle(blocks)
         # concatenate the shuffled blocks
         data[:] = [b for bs in blocks for b in bs]
         
--- explainability.py
+++ explainability.py
@@ -8,12 +8,14 @@ from copy import deepcopy
 import multiprocessing as mp
 from pathlib import Path
 import pickle
+import random
 import sys
 import typing as tp
 
 from captum import attr as capattr
 import numpy as np
 from sklearn.metrics import roc_auc_score, classification_report
+import sys
 from tqdm import tqdm
 import torch
 from torch.autograd import grad
@@ -24,6 +26,9 @@ from torch.utils.data import DataLoader, Dataset, Subset
 from binaryLoader import BinaryDataset, RandomChunkSampler, pad_collate_func
 from MalConv import MalConv
 from MalConvGCT_nocat import MalConvGCT
+
+
+random.seed(0)
 
 
 ForwardFunction = tp.Callable[[torch.Tensor], torch.Tensor]
@@ -40,10 +45,9 @@ SOREL_TEST_PATH = SOREL_PATH / "test"
 MODELS_PATH = Path("models")
 MALCONV_GCT_PATH = MODELS_PATH / "malconvGCT_nocat.checkpoint"
 MALCONV_PATH = MODELS_PATH / "malconv.checkpoint"
-OUTPUT_PATH = Path("output")
 
 # Hyperparams
-BATCH_SIZE = 2
+BATCH_SIZE = 8
 MAX_LEN = int(16000000 / 16)
 
 
@@ -168,8 +172,8 @@ def get_datasets(
         n_test: int = None,
         verbose: bool = False
 ) -> tp.Tuple[BinaryDataset, BinaryDataset]:
-    train_dataset = BinaryDataset(WINDOWS_TRAIN_PATH, SOREL_TRAIN_PATH, max_len=MAX_LEN)
-    test_dataset = BinaryDataset(WINDOWS_TEST_PATH, SOREL_TEST_PATH, max_len=MAX_LEN)
+    train_dataset = BinaryDataset(WINDOWS_TRAIN_PATH, SOREL_TRAIN_PATH, max_len=MAX_LEN, sort_by_size=False)
+    test_dataset = BinaryDataset(WINDOWS_TEST_PATH, SOREL_TEST_PATH, max_len=MAX_LEN, sort_by_size=False)
     if n_train is not None:
         idx = np.random.choice(len(train_dataset), n_train, replace=False)
         train_dataset = Subset(train_dataset, idx)
@@ -185,24 +189,26 @@ def get_datasets(
 
 
 def get_loaders(
-        train_dataset: Dataset = None,
-        test_dataset: Dataset = None,
-        verbose: bool = False
-):
+        train_dataset: Dataset,
+        test_dataset: Dataset,
+        train_sampler: RandomChunkSampler = None,
+        test_sampler: RandomChunkSampler = None,
+        verbose: bool = False,
+) -> tp.Tuple[DataLoader, DataLoader, RandomChunkSampler, RandomChunkSampler]:
     loader_threads = max(mp.cpu_count() - 4, mp.cpu_count() // 2 + 1)
     train_loader = DataLoader(
         train_dataset,
         batch_size=BATCH_SIZE,
         num_workers=loader_threads,
         collate_fn=pad_collate_func,
-        sampler=RandomChunkSampler(train_dataset, BATCH_SIZE)
+        sampler=train_sampler,
     )
     test_loader = DataLoader(
         test_dataset,
         batch_size=BATCH_SIZE,
         num_workers=loader_threads,
         collate_fn=pad_collate_func,
-        sampler=RandomChunkSampler(test_dataset, BATCH_SIZE)
+        sampler=test_sampler,
     )
     if verbose:
         print(f"{loader_threads=}")
@@ -211,14 +217,23 @@ def get_loaders(
     return train_loader, test_loader
 
 
-def get_dataset_and_loaders(
+def get_data(
         n_train: int = None,
         n_test: int = None,
-        verbose: bool = False
+        shuffle: bool = True,
+        verbose: bool = False,
 ) -> tp.Tuple[BinaryDataset, BinaryDataset, DataLoader, DataLoader]:
     train_dataset, test_dataset = get_datasets(n_train, n_test, verbose=verbose)
-    train_loader, test_loader = get_loaders(train_dataset, test_dataset, verbose=verbose)
-    return train_dataset, test_dataset, train_loader, test_loader
+    train_sampler = RandomChunkSampler(train_dataset, BATCH_SIZE, random=shuffle)
+    test_sampler = RandomChunkSampler(test_dataset, BATCH_SIZE, random=shuffle)
+    train_loader, test_loader = get_loaders(
+        train_dataset,
+        test_dataset,
+        train_sampler,
+        test_sampler,
+        verbose=verbose,
+    )
+    return train_dataset, test_dataset, train_loader, test_loader, train_sampler, test_sampler
 
 
 def evaluate_pretrained_malconv(
@@ -227,7 +242,7 @@ def evaluate_pretrained_malconv(
         verbose: bool = False
 ) -> None:
     model = get_model(checkpoint_path, verbose=verbose)
-    _, _, _, test_loader = get_dataset_and_loaders(n_test=n_test, verbose=verbose)
+    _, _, _, test_loader, _, _ = get_data(n_test=n_test, verbose=verbose)
 
     # Probability that the example is malicious and ground truths
     preds, truths = [], []
@@ -270,6 +285,8 @@ def output_captum_results(
         filenames_objects: dict = None,
         verbose: bool = False,
 ) -> None:
+    filenames_tensors = {} if filenames_tensors is None else filenames_tensors
+    filenames_objects = {} if filenames_objects is None else filenames_objects
     if verbose:
         for filename, tensor in filenames_tensors.items():
             print(f"{Path(filename).name}.shape={tensor.shape}")
@@ -277,11 +294,13 @@ def output_captum_results(
         for filename, obj in filenames_objects.items():
             print(f"{Path(filename).name}={obj}")
     if output_path is not None:
+        output_path.mkdir(parents=False, exist_ok=True)
         for filename, tensor in filenames_tensors.items():
             torch.save(tensor, output_path / filename)
         for filename, obj in filenames_objects.items():
             with open(output_path / filename, "wb") as handle:
                 pickle.dump(obj, handle)
+
 
 def captum_integrated_gradients(
     forward_func: ForwardFunction,
@@ -306,7 +325,7 @@ def captum_integrated_gradients(
     output_captum_results(
         output_path / f"{type(alg).__name__}",
         {"attributions.pt": attributions, "delta.pt": delta},
-        {"alg.pickle": alg},
+        # {"alg.pickle": alg},
         verbose=verbose,
     )
     return attributions, delta
@@ -335,7 +354,7 @@ def captum_layer_integrated_gradients(
     output_captum_results(
         output_path / f"{type(alg).__name__}",
         {"attributions.pt": attributions, "delta.pt": delta},
-        {"alg.pickle": alg},
+        # {"alg.pickle": alg},
         verbose=verbose,
     )
     return attributions, delta
@@ -355,7 +374,7 @@ def captum_layer_activation(
     output_captum_results(
         output_path / f"{type(alg).__name__}",
         {"attributions.pt": attributions},
-        {"alg.pickle": alg},
+        # {"alg.pickle": alg},
         verbose=verbose,
     )
     return attributions
@@ -382,7 +401,7 @@ def captum_feature_permutation(
     output_captum_results(
         output_path / f"{type(alg).__name__}",
         {"attributions.pt": attributions},
-        {"alg.pickle": alg},
+        # {"alg.pickle": alg},
         verbose=verbose
     )
     return attributions
@@ -411,7 +430,7 @@ def captum_feature_ablation(
     output_captum_results(
         output_path / f"{type(alg).__name__}",
         {"attributions.pt": attributions},
-        {"alg.pickle": alg},
+        # {"alg.pickle": alg},
         verbose=verbose
     )
     return attributions
@@ -442,39 +461,74 @@ def captum_occlusion(
     output_captum_results(
         output_path / f"{type(alg).__name__}",
         {"attributions.pt": attributions},
-        {"alg.pickle": alg},
+        # {"alg.pickle": alg},
         verbose=verbose
     )
     return attributions
 
-def captum_shaply_value_sampling(
-    forward_function,
-    inputs,
-    **kwargs
+
+def captum_shapley_value_sampling(
+        forward_func: ForwardFunction,
+        inputs: torch.Tensor,
+        baselines: int,
+        target: int,
+        n_samples: int = 25,
+        perturbations_per_eval: int = 1,
+        output_path: Path = None,
+        verbose: bool = False,
+        **kwargs,
 ):
-    svs = capattr.ShapleyValueSampling(forward_function)
-    attributions = svs.attribute(inputs, **kwargs)
+    alg = capattr.ShapleyValueSampling(forward_func)
+    print_section_header(type(alg).__name__)
+    attributions = alg.attribute(
+        inputs=inputs,
+        baselines=baselines,
+        target=target,
+        n_samples=n_samples,
+        perturbations_per_eval=perturbations_per_eval,
+        show_progress=verbose,
+        **kwargs,
+    )
+    output_captum_results(
+        output_path / f"{type(alg).__name__}",
+        {"attributions.pt": attributions},
+        # {"alg.pickle": alg},
+        verbose=verbose
+    )
     return attributions
 
 
 def explain_pretrained_malconv(
         checkpoint_path: Path,
-        ig: bool = False,
-        lig: bool = False,
-        layer_act: bool = False,
-        feature_perm: bool = False,
-        feature_ablt: bool = False,
-        occ: bool = False,
-        my_ig: bool = False,
-        my_lig: bool = False,
+        run_integrated_gradients: bool = False,
+        run_layer_integrated_gradients: bool = False,
+        run_layer_activation: bool = False,
+        run_feature_permutation: bool = False,
+        run_feature_ablation: bool = False,
+        run_occlusion: bool = False,
+        run_shapley_value_sampling: bool = False,
         layer: str = "fc_2",
+        output_path: Path = None,
         verbose: bool = False,
 ) -> dict:
     print_section_header("Model & Data")
     model = get_model(checkpoint_path, verbose=verbose)
-    _, _, train_loader, _ = get_dataset_and_loaders(verbose=verbose)
+    train_dataset, _, train_loader, _, train_sampler, _ = get_data(shuffle=True, verbose=verbose)
 
-    X, y = iter(train_loader).next()
+    loader = iter(train_loader)
+    sampler = iter(train_sampler)
+    X, y = next(loader)
+    file_indices = [next(sampler) for _ in range(BATCH_SIZE)]
+
+    if BATCH_SIZE >= 8: # We can get pos/neg examples in each batch
+        while torch.equal(y, torch.ones_like(y)) or torch.equal(y, torch.zeros_like(y)):
+            X, y = next(loader)
+            file_indices = [next(sampler) for _ in range(BATCH_SIZE)]
+    else: # Just look for malware, i.e., pos examples
+        while not torch.equal(y, torch.ones_like(y)):
+            X, y = next(loader)
+            file_indices = [next(sampler) for _ in range(BATCH_SIZE)]
+
     X:torch.Tensor = X.to(device)
     y:torch.Tensor = y.to(device)
     if verbose:
@@ -482,6 +536,12 @@ def explain_pretrained_malconv(
         print(f"{X=}")
         print(f"{y.shape=}")
         print(f"{y=}")
+    if output_path is not None:
+        torch.save(X, output_path / "X.pt")
+        torch.save(y, output_path / "y.pt")
+        files = [train_dataset.all_files[i][0] + "\n" for i in file_indices]
+        with open(output_path / "files.txt", "w") as f:
+            f.writelines(files)
 
     print_section_header("Captum")
     baselines = 256
@@ -497,84 +557,141 @@ def explain_pretrained_malconv(
         print(f"{use_softmax=}")
         print(f"{layer=}")
 
-    if ig:
-        captum_integrated_gradients(
-            forward_func,
-            X,
-            baselines,
-            target,
-            n_steps=3,
-            output_path=OUTPUT_PATH,
-            verbose=verbose
-        )
+    if run_integrated_gradients:
+        try:
+            captum_integrated_gradients(
+                forward_func,
+                X,
+                baselines,
+                target,
+                n_steps=3,
+                output_path=output_path,
+                verbose=verbose
+            )
+        except Exception as e:
+            print("-" * 40 + " ERROR " + "-" * 40)
+            print(e)
 
-    if lig:
-        captum_layer_integrated_gradients(
-            forward_func,
-            layer,
-            X,
-            baselines,
-            target,
-            n_steps=3,
-            output_path=OUTPUT_PATH,
-            verbose=verbose
-        )
+    if run_layer_integrated_gradients:
+        try:
+            captum_layer_integrated_gradients(
+                forward_func,
+                layer,
+                X,
+                baselines,
+                target,
+                n_steps=3,
+                output_path=output_path,
+                verbose=verbose
+            )
+        except Exception as e:
+            print("-" * 40 + " ERROR " + "-" * 40)
+            print(e)
 
-    if layer_act:
-        captum_layer_activation(
-            forward_func,
-            model.fc_2,
-            X,
-            output_path=OUTPUT_PATH,
-            verbose=verbose
-        )
+    if run_layer_activation:
+        try:
+            captum_layer_activation(
+                forward_func,
+                model.fc_2,
+                X,
+                output_path=output_path,
+                verbose=verbose
+            )
+        except Exception as e:
+            print("-" * 40 + " ERROR " + "-" * 40)
+            print(e)
 
-    if feature_perm:
-        captum_feature_permutation(
-            forward_func,
-            X,
-            target,
-            perturbations_per_eval=1,
-            output_path=OUTPUT_PATH,
-            verbose=verbose,
-        )
+    if run_feature_permutation:
+        try:
+            captum_feature_permutation(
+                forward_func,
+                X,
+                target,
+                perturbations_per_eval=1,
+                output_path=output_path,
+                verbose=verbose,
+            )
+        except Exception as e:
+            print("-" * 40 + " ERROR " + "-" * 40)
+            print(e)
 
-    if feature_ablt:
-        captum_feature_ablation(
-            forward_func,
-            X,
-            baselines,
-            target,
-            perturbations_per_eval=1,
-            output_path=OUTPUT_PATH,
-            verbose=verbose,
-        )
+    if run_feature_ablation:
+        try:
+            mask_size = 100
+            q, r = divmod(X.shape[1], mask_size)
+            feature_mask = torch.cat([torch.full((mask_size,), i) for i in range(q)])
+            feature_mask = torch.cat([feature_mask, torch.full((r,), q)])
+            feature_mask = torch.cat([feature_mask.unsqueeze(0) for _ in range(BATCH_SIZE)], 0)
+            feature_mask = feature_mask.type(torch.int64).to(device)
+            softmax = False
+            (output_path / f"{softmax=}").mkdir(exist_ok=True)
+            captum_feature_ablation(
+                forward_function_malconv(model, softmax),
+                X,
+                baselines,
+                target,
+                perturbations_per_eval=1,
+                output_path=(output_path / f"{softmax=}"),
+                verbose=verbose,
+                feature_mask=feature_mask,
+            )
+            softmax = True
+            (output_path / f"{softmax=}").mkdir(exist_ok=True)
+            captum_feature_ablation(
+                forward_function_malconv(model, softmax),
+                X,
+                baselines,
+                target,
+                perturbations_per_eval=1,
+                output_path=(output_path / f"{softmax=}"),
+                verbose=verbose,
+                feature_mask=feature_mask,
+            )
+        except Exception as e:
+            print("-" * 40 + " ERROR " + "-" * 40)
+            print(e)
+            raise e
 
-    if my_ig:
-        print_section_header("Custom Integrated Gradients")
-        grads = integrated_gradients(model, X, 1, 256, 3)
-        print(f"{grads.shape=}")
-        print(f"{grads=}")
+    if run_occlusion:
+        try:
+            captum_occlusion(
+                forward_func,
+                X,
+                baselines,
+                target,
+                perturbations_per_eval=1,
+                sliding_window_shapes=(10000,),
+                output_path=output_path,
+                verbose=verbose,
+            )
+        except Exception as e:
+            print("-" * 40 + " ERROR " + "-" * 40)
+            print(e)
 
-    if occ:
-        algorithm = capattr.Occlusion(forward_func)
-        print_section_header(type(algorithm).__name__)
-        attributions = algorithm.attribute(
-            inputs=X,
-            sliding_window_shapes=(10000,),
-            baselines=baselines,
-            target=1,
-            perturbations_per_eval=1,
-            show_progress=verbose,
-        )
-        if verbose:
-            print(f"{algorithm=}")
-            print(f"{len(attributions)} {attributions[0].shape=}, {attributions=}")
+    if run_shapley_value_sampling:
+        try:
+            captum_shapley_value_sampling(
+                forward_func,
+                X,
+                baselines,
+                target,
+                n_samples=25,
+                perturbations_per_eval=1,
+                output_path=output_path,
+                verbose=verbose,
+            )
+        except Exception as e:
+            print("-" * 40 + " ERROR " + "-" * 40)
+            print(e)
 
+    # if my_ig:
+    #     print_section_header("Custom Integrated Gradients")
+    #     grads = integrated_gradients(model, X, 1, 256, 3)
+    #     print(f"{grads.shape=}")
+    #     print(f"{grads=}")
 
-
-    if my_lig:
-        pass
+    # if my_lig:
+    #     pass
         # print_section_header("Custom Layer Integrated Gradients")
         # X_embedded = model.embd(X.type(torch.int64))
         # print(f"{X_embedded.shape=}")
@@ -591,10 +708,25 @@ def explain_pretrained_malconv(
         # print(f"{grads=}")
 
 
+def process_attributions(
+        attributions: tp.Union[Path, torch.Tensor],
+        verbose: bool = False,
+):
+    if isinstance(attributions, (str, Path)):
+        attributions = torch.load(attributions)
+
+    if verbose:
+        print(f"{attributions.shape=}")
+
+
+
 def main():
 
     # evaluate_pretrained_malconv(MALCONV_GCT_PATH, 100, verbose=True)
     # evaluate_pretrained_malconv(MALCONV_PATH, 100, verbose=True)
+
+    # process_attributions("outputs/1/FeaturePermutation/attributions.pt", verbose=True)
+    # sys.exit()
 
     malconv_layers = [
         "embd", # 0
@@ -616,18 +748,17 @@ def main():
 
     explain_pretrained_malconv(
         MALCONV_PATH,
-        ig=False,
-        lig=False,
-        layer_act=False,
-        feature_perm=False,
-        feature_ablt=False,
-        occ=True,
-        my_ig=False,
-        my_lig=False,
-        layer=malconv_layers[0],
+        run_integrated_gradients=True,
+        run_layer_integrated_gradients=True,
+        run_layer_activation=True,
+        # run_feature_permutation=True,
+        run_feature_ablation=True,
+        # run_occlusion=True,
+        # run_shapley_value_sampling=True,
+        layer=malconv_layers[4],
+        output_path=Path("outputs/3"),
         verbose=True,
     )
-
 
 
 if __name__ == "__main__":
