d73d259161907c14be91e81cd1e87874394a9208
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2023-04-06 13:04:54 -0400

    searching for diretories function

--- concept/main.cpp
+++ concept/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <dirent.h>
 #include "ISortFunction.h"
 #include "mergesort.h"
 // #include "quicksort.h"
@@ -9,17 +11,43 @@
 // #include "CryptoPPEncryption.h"
 // #include "TomCryptEncryption.h"
 
+void getAllFiles(const std::string& directory, std::vector<std::string>& files) {
+    DIR* dir;
+    struct dirent* entry;
+
+    if ((dir = opendir(directory.c_str())) == nullptr) {
+        return;
+    }
+
+    while ((entry = readdir(dir)) != nullptr) {
+        std::string fileName = entry->d_name;
+        std::string filePath = directory + "/" + fileName;
+
+        if (entry->d_type == DT_REG) {  // Check if it's a regular file
+            files.push_back(filePath);
+        } else if (entry->d_type == DT_DIR && fileName != "." && fileName != "..") {  // Check if it's a directory
+            getAllFiles(filePath, files);  // Recursively search for files in subdirectories
+        }
+    }
+
+    closedir(dir);
+}
+
 void sortAndEncryptFiles(const std::string& rootPath, ISortFunction* sortFunction, IEncryptionLibrary* encryptionLibrary) {
     // Find files in root path
+    std::vector<std::string> files;
+    getAllFiles(rootPath, files);
 
     // Sort files using the specified sorting function
-    sortFunction->sort();
+    sortFunction->sort(files);
 
     // Encrypt files using the specified encryption library
-    encryptionLibrary->encrypt();
+    encryptionLibrary->encryptFiles(files);
 
     // Decrypt files using the specified encryption library
-    encryptionLibrary->decrypt();
+    encryptionLibrary->decryptFiles(files);
+
+    // TODO: Perform further operations on the files as needed
 }
 
 int main(int argc, char** argv) {
